0e6979b8fd03c850f54586a08c0bbe1d4ae5bcaf
Albert Young <albertkyoung@yahoo.com>
2018-01-28 19:55:51 -0800

    Add functionality to CeeseMVC

---
+++ HelloWorld/Controllers/HelloWorldBonjourController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldBonjourController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ListAndStrings/Program.cs
+++ ListAndStrings/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ListAndStrings
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Hello World! Enter your name:"); //prompts for users name
+            String line = Console.ReadLine(); //Gets a string from the user
+            Console.Write("Hello " + line); //Tells the user hello
+            Console.ReadLine();
         }
     }
 }
